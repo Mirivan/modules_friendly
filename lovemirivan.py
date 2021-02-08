7f39c9366389bcbbfe21d721a34062426bca2981
Mirivan <54078891+Mirivan@users.noreply.github.com>
2021-02-08 21:59:20 +0500

    Add files via upload

---
+++ lovemirivan.py
@@ -0,0 +1,16 @@
+from .. import loader
+from asyncio import sleep
+from collections import deque
+@loader.tds
+class LoveMirMod(loader.Module):
+	strings = {"name": "Love (Mirivan)"}
+	@loader.owner
+	async def lovemircmd(self, message):
+		deq = deque(list('❤️🧡💛💚💙💜🖤🤍'))
+        try:
+            for _ in range(48):
+                await asyncio.sleep(0.5)
+                await event.edit(''.join(deq))
+                deq.rotate(1)
+        except Exception:
+        	pass
